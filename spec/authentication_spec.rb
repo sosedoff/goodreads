436b0ccc3ae3100b58efde2d627d38f5c4af9940
Marc Cooper <git@auxbuss.com>
2012-01-25 16:08:12 +0000

    remove class variable

--- spec/authentication_spec.rb
+++ spec/authentication_spec.rb
@@ -10,11 +10,10 @@ describe 'Client' do
   end
     
   it 'should raise Goodreads::Unauthorized if API key is not valid' do
-    Goodreads.configure('INVALID_KEY')
+    @client.configure(:api_key => 'INVALID_KEY')
     stub_request(:get, "http://www.goodreads.com/book/isbn?format=xml&isbn=054748250711&key=INVALID_KEY").
       to_return(:status => 401)
       
     proc { @client.book_by_isbn('054748250711') }.should raise_error Goodreads::Unauthorized
   end
 end
-
