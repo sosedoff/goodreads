4030367bffd5f08c2d12e6b43c396b77d67369b3
Dan Sosedoff <dan.sosedoff@gmail.com>
2012-01-25 20:04:32 -0600

    Global configuration, specs

--- lib/goodreads.rb
+++ lib/goodreads.rb
@@ -4,9 +4,32 @@ require 'goodreads/request'
 require 'goodreads/client'
 
 module Goodreads
+  @@options = {}
+  
   # Create a new Goodreads::Client instance
   #
   def self.new(options={})
     Goodreads::Client.new(options)
+  end
+  
+  # Define a global configuration
+  #
+  # options[:api_key]    - Account API key
+  # options[:api_secret] - Account API secret
+  #
+  def self.configure(options={})
+    unless options.kind_of?(Hash)
+      raise ArgumentError, "Options hash required."
+    end
+    
+    @@options[:api_key]    = options[:api_key]
+    @@options[:api_secret] = options[:api_secret]
+    @@options
+  end
+  
+  # Returns global configuration hash
+  #
+  def self.configuration
+    @@options
   end
 end
--- spec/goodreads_spec.rb
+++ spec/goodreads_spec.rb
@@ -6,4 +6,38 @@ describe 'Goodreads' do
       Goodreads.new.should be_a Goodreads::Client
     end
   end
+  
+  context '.configure' do
+    it 'sets a global configuration options' do
+      r = Goodreads.configure(:api_key => 'FOO', :api_secret => 'BAR')
+      r.should be_a Hash
+      r.should have_key(:api_key)
+      r.should have_key(:api_secret)
+      r[:api_key].should eql('FOO')
+      r[:api_secret].should eql('BAR')
+    end
+    
+    it 'raises ConfigurationError on invalid config parameter' do
+      proc { Goodreads.new(nil) }.
+        should raise_error ArgumentError, "Options hash required."
+      
+      proc { Goodreads.new('foo') }.
+        should raise_error ArgumentError, "Options hash required."
+    end
+  end
+  
+  context '.configuration' do
+    before do
+      Goodreads.configure(:api_key => 'FOO', :api_secret => 'BAR')
+    end
+    
+    it 'returns global configuration options' do
+      r = Goodreads.configuration
+      r.should be_a Hash
+      r.should have_key(:api_key)
+      r.should have_key(:api_secret)
+      r[:api_key].should eql('FOO')
+      r[:api_secret].should eql('BAR')
+    end
+  end
 end
