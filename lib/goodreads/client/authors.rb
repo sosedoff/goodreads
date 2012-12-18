def3b83815915bd46f891f0b9755aa112486b030
Adam Carroll <acarr468@gmail.com>
2012-12-18 11:19:05 -0500

    Added documentation

--- lib/goodreads/client/authors.rb
+++ lib/goodreads/client/authors.rb
@@ -8,12 +8,16 @@ module Goodreads
       Hashie::Mash.new(data['author'])
     end
 
+    # Get ID number of author
+    #
     def authorID(id, params={})
       params[:id] = id
       data = request('/api/author_url', params)
       Hashie::Mash.new(data['author'])
     end
 
+    # Get list of books by author
+    #
     def authorList(id, params={})
       params[:id] = id
       data = request('/author/list', params)
