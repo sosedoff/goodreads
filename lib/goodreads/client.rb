9e54fdc08966fb40df1db5ddf3128e59dee1dfcc
Roderick Monje <rod@foveacentral.com>
2013-02-23 18:32:11 -0600

    new clients default to global config

--- lib/goodreads/client.rb
+++ lib/goodreads/client.rb
@@ -17,9 +17,9 @@ module Goodreads
     include Goodreads::Shelves
     include Goodreads::Authorized
     include Goodreads::Groups
-    
+
     attr_reader :api_key, :api_secret, :oauth_token
-    
+
     # Initialize a Goodreads::Client instance
     #
     # options[:api_key]     - Account API key
@@ -30,9 +30,9 @@ module Goodreads
       unless options.kind_of?(Hash)
         raise ArgumentError, "Options hash required."
       end
-      
-      @api_key    = options[:api_key]
-      @api_secret = options[:api_secret]
+
+      @api_key    = options[:api_key] || Goodreads.configuration[:api_key]
+      @api_secret = options[:api_secret] || Goodreads.configuration[:api_secret]
       @oauth_token = options[:oauth_token]
     end
   end
