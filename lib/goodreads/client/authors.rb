b03d987b79c560c11ebb71d8455d1e7d654e4c68
Jon Doud <jon.doud@gmail.com>
2014-04-26 12:28:40 -0600

    added books_by_author method

--- lib/goodreads/client/authors.rb
+++ lib/goodreads/client/authors.rb
@@ -15,5 +15,14 @@ module Goodreads
       data = request('/api/author_url', params)
       Hashie::Mash.new(data['author'])
     end
+
+    # Get an author's list of books by author id
+    #
+    def books_by_author(id, params={})
+      params[:id] = id
+      data = request('/author/list', params)
+      Hashie::Mash.new(data['author'])
+    end
+
   end
 end
