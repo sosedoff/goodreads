f59d690b0b5bb71295306b34b789cd1437f7a6b2
Jon Doud <jon.doud@gmail.com>
2014-04-28 08:18:52 -0600

    Added pagination support to #owned_books

--- lib/goodreads/client/ownership.rb
+++ lib/goodreads/client/ownership.rb
@@ -2,8 +2,17 @@ module Goodreads
   module Ownership
 
     # Get list of owned books
-    def owned_books(id)
-      data = oauth_request('/owned_books/user', :id => id)
+    #
+    # Get goodreads book id: client.owned_books(user_id)[i].book.id
+    # Get book title: client.owned_books(user_id)[i].book.title
+    # Get book author: client.owned_books(user_id)[i].book.authors.author.name
+    # Get book owned?: client.owned_books(user_id)[i].review.owned
+    # Get shelf of book: client.owned_books(user_id)[i].review.shelves.shelf.name
+    # Get ISBN13: client.owned_books(user_id)[i].book.isbn13
+    # Get number of pages: client.owned_books(user_id)[i].book.num_pages
+    def owned_books(id, params={})
+      params[:id] = id
+      data = oauth_request('/owned_books/user', params)
       owned_books = data['owned_books']['owned_book']
 
       books = []
