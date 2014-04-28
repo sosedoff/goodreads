c3deff2c9b2fd0aa73536106ddb448b657db1825
Jon Doud <jon.doud@gmail.com>
2014-04-28 11:22:31 -0600

    Fixed #books_by_author.  Added #series_by_author.

--- lib/goodreads/client/authors.rb
+++ lib/goodreads/client/authors.rb
@@ -21,7 +21,27 @@ module Goodreads
     def books_by_author(id, params={})
       params[:id] = id
       data = request('/author/list', params)
-      Hashie::Mash.new(data['author'])
+      book_list = data['author']['books']['book']
+
+      books = []
+      books = book_list.map {|b| Hashie::Mash.new(b)}
+
+      Hashie::Mash.new({
+        :start => data['author']['books']['start'].to_i,
+        :end => data['author']['books']['end'].to_i,
+        :total => data['author']['books']['total'].to_i,
+        :books => books
+      }) 
+    end
+
+    # Get the list of series for an author
+    #
+    def series_by_author(id)
+      data = request('/series/list', {:id => id})
+      series_list = data['series_works']
+
+      series = []
+      series = series_list.map {|s| Hashie::Mash.new(s)}
     end
 
   end
