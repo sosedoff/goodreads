a2d07534dc0773d6129bdb81af53f5d3f16a6897
Jon Doud <jon.doud@gmail.com>
2014-04-29 21:21:12 -0700

    Adding tests for new methods

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -165,6 +165,59 @@ describe 'Client' do
     end
   end
 
+  describe '#books_by_author' do
+    before { stub_with_key_get('/author/list', {:id => '18541'}, 'books_by_author.xml') }
+
+    it 'returns list of books for a specified author' do
+      list = client.books_by_author('18541')
+
+      list.should respond_to :start
+      list.should respond_to :end
+      list.should respond_to :total
+      list.should respond_to :books
+
+      list.start.should eq 1
+      list.end.should eq 30
+      list.total.should eq 50
+      list.books.length.should eq 30
+      list.books.first.id.should eq 104744
+      list.books.first.title.strip.should eq 'The Cathedral & the Bazaar: Musings on Linux and Open Source by an Accidental Revolutionary'
+    end
+  end
+
+  describe '#series_by_author' do
+    before { stub_with_key_get('/series/list', {:id => '227840'}, 'series_by_author.xml') }
+
+    it 'returns list of series for a specified author' do
+      list = client.series_by_author('227840')
+
+      list.length.should eq 13
+      list.first.series.title.strip.should eq 'The Ballet Family'
+      list.first.series.primary_work_count.should eq "2"
+      list.first.series.series_works_count.should eq "2"
+    end
+  end
+
+  describe '#books_by_series' do
+    before { stub_with_key_get('/series/show', {:id => '52928'}, 'books_by_series.xml') }
+
+    it 'returns list of books for a specified author' do
+      list = client.books_by_series('52928')
+
+      list.should respond_to :book_count
+      list.should respond_to :series_title
+      list.should respond_to :series
+      list.should respond_to :titles
+
+      list.book_count.should eq "3"
+      list.series_title.strip.should eq 'Matched'
+      list.titles.should eq ["Matched (Matched, #1)", "Crossed (Matched, #2)", "Reached (Matched, #3)", "Matched Trilogy Box Set"]
+      list.series.length.should eq 4
+      list.series.first.id.should eq "206283"
+      list.series.first.work.best_book.title.strip.should eq "Matched (Matched, #1)"
+    end
+  end
+
   describe '#user' do
     before { stub_with_key_get('/user/show', {:id => '878044'}, 'user.xml') }
 
@@ -246,6 +299,79 @@ describe 'Client' do
     end
   end
 
+  describe '#list_shelves' do
+    it 'returns list of shelves for a specified user' do  
+      stub_with_key_get('/shelf/list.xml', {:user_id => '66726'}, 'list_shelves.xml')
+
+      shelves = client.list_shelves(66726)
+
+      shelves.should respond_to :start
+      shelves.should respond_to :end
+      shelves.should respond_to :total
+      shelves.should respond_to :names
+      shelves.should respond_to :shelves
+
+      shelves.start.should eq 1
+      shelves.end.should eq 6
+      shelves.total.should eq 6
+      shelves.names.should eq ["read", "currently-reading", "to-read", "to-be-processed", "owned", "want-to-own"]
+      shelves.shelves.last.name.should eq 'want-to-own'
+    end
+  end
+
+  describe '#add_book_to_shelf' do
+    let(:consumer) { OAuth::Consumer.new('API_KEY', 'SECRET_KEY', :site => 'http://www.goodreads.com') }
+    let(:token)    { OAuth::AccessToken.new(consumer, 'ACCESS_TOKEN', 'ACCESS_SECRET') }
+
+    before do
+      stub_request(:post, "http://www.goodreads.com/shelf/add_to_shelf.xml?book_id=1157446&name=to-read").
+        to_return(:status => 200, :body => fixture('add_book_to_shelf.xml'), :headers => {})
+    end
+
+    it 'returns success after adding book to a shelf' do
+      client = Goodreads::Client.new(:api_key => 'SECRET_KEY', :oauth_token => token)
+        data = client.add_book_to_shelf(1157446, 'to-read')
+
+        data.body.should match('<name>to-read</name>')
+        data.body.should match('<position type="integer">1</position>')
+    end
+  end
+
+  describe '#add_books_to_shelves' do
+    let(:consumer) { OAuth::Consumer.new('API_KEY', 'SECRET_KEY', :site => 'http://www.goodreads.com') }
+    let(:token)    { OAuth::AccessToken.new(consumer, 'ACCESS_TOKEN', 'ACCESS_SECRET') }
+
+    before do
+      stub_request(:post, "http://www.goodreads.com/shelf/add_books_to_shelves.xml?bookids=1157446,133376,17311580&shelves=to-read,want-to-own").
+        to_return(:status => 200, :body => fixture('add_books_to_shelves.xml'), :headers => {})
+    end
+
+    it 'returns success after adding books to shelves' do
+      client = Goodreads::Client.new(:api_key => 'SECRET_KEY', :oauth_token => token)
+        data = client.add_books_to_shelves("1157446,133376,17311580", 'to-read,want-to-own')
+
+        data.body.should match('<result>ok</result>')
+        data.body.should match('shelf_add_books_to_shelves')
+    end
+  end
+
+  describe '#remove_book_from_shelf' do
+    let(:consumer) { OAuth::Consumer.new('API_KEY', 'SECRET_KEY', :site => 'http://www.goodreads.com') }
+    let(:token)    { OAuth::AccessToken.new(consumer, 'ACCESS_TOKEN', 'ACCESS_SECRET') }
+
+    before do
+      stub_request(:post, "http://www.goodreads.com/shelf/add_to_shelf.xml?book_id=1157446&name=to-read").
+        to_return(:status => 200, :body => fixture('remove_book_from_shelf.xml'), :headers => {})
+    end
+
+    it 'returns empty message after removing book from a shelf' do
+      client = Goodreads::Client.new(:api_key => 'SECRET_KEY', :oauth_token => token)
+        data = client.add_book_to_shelf(1157446, 'to-read')
+
+        data.body.should eq nil
+    end
+  end
+
   describe '#user_id' do
     let(:consumer) { OAuth::Consumer.new('API_KEY', 'SECRET_KEY', :site => 'http://www.goodreads.com') }
     let(:token)    { OAuth::AccessToken.new(consumer, 'ACCESS_TOKEN', 'ACCESS_SECRET') }
@@ -293,4 +419,42 @@ describe 'Client' do
       group_list.group[2].users_count.should eq '530'
     end
   end
+
+  describe '#owned_books' do
+    let(:consumer) { OAuth::Consumer.new('API_KEY', 'SECRET_KEY', :site => 'http://www.goodreads.com') }
+    let(:token)    { OAuth::AccessToken.new(consumer, 'ACCESS_TOKEN', 'ACCESS_SECRET') }
+
+    before do
+      stub_request(:get, "http://www.goodreads.com/owned_books/user?id=6921131").
+        to_return(:status => 200, :body => fixture('owned_books.xml'), :headers => {})
+    end
+
+    it 'returns list of books the specified user owns' do
+      client = Goodreads::Client.new(:api_key => 'SECRET_KEY', :oauth_token => token)
+      books = client.owned_books(6921131)
+
+      books.size.should eq 30
+      books.first.current_owner_id.should eq 6921131
+      books.first.book.isbn13.should eq "9781741856255"
+    end
+  end  
+
+  describe '#add_ownership' do
+    let(:consumer) { OAuth::Consumer.new('API_KEY', 'SECRET_KEY', :site => 'http://www.goodreads.com') }
+    let(:token)    { OAuth::AccessToken.new(consumer, 'ACCESS_TOKEN', 'ACCESS_SECRET') }
+
+    before do
+      stub_request(:post, "http://www.goodreads.com/owned_books.xml?owned_book%5Bbook_id%5D=1157446").
+        to_return(:status => 200, :body => fixture('add_ownership.xml'), :headers => {})
+    end
+
+    it 'returns success after adding the book' do
+      client = Goodreads::Client.new(:api_key => 'SECRET_KEY', :oauth_token => token)
+        data = client.add_ownership(1157446)
+
+        data.body.should match('<current-owner-id type="integer">6961231</current-owner-id>')
+        data.body.should match('<book-id type="integer">1157446</book-id>')
+    end
+  end
+
 end
